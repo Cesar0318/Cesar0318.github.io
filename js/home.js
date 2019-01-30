54c6f5646612d17070b5c62f52c6740de36810c8
Cesar0318 <Cesar0318@users.noreply.github.com>
2019-01-30 13:39:16 -0500

    a lot

--- js/home.js
+++ js/home.js
@@ -4,9 +4,9 @@ $(document).ready(function(){
     
     $('.container .slides:gt(0)').hide();
     setInterval(function(){
-      $('.container :first-child').fadeOut(1000).next('.slides').fadeIn(2200)
-      .end().appendTo('.container');
-  	}, 2200);
+      $('.container :first-child').fadeOut(1200).next('.slides').fadeIn(2200)
+      .end(1000).appendTo('.container');
+  	}, 3000);
     
   });
  });
